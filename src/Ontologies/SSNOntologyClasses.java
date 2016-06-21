866e3b70b5b968990ec795fa8a50c7035ea05472
HatemMorgan <hatemmorgan17@gmail.com>
2016-06-21 12:46:59 +0200

    adding SSNOntologyClasses that return all the needed classes in SSN Ontology

--- src/Ontologies/SSNOntologyClasses.java
+++ src/Ontologies/SSNOntologyClasses.java
@@ -17,7 +17,33 @@ public class SSNOntologyClasses {
 		return system;
 	}
 	
+	public static OntClass getDeviceClass(){
+		OntClass Device = SSNOntologyModel.getOntClass(SSN_URI+"Device");
+		return Device;
+	}
+	
+	public static OntClass getSensingDeviceClass(){
+		OntClass SensingDevice = SSNOntologyModel.getOntClass(SSN_URI+"SensingDevice");
+		return SensingDevice;
+	}
+	
+	public static OntClass getSensorClass(){
+		OntClass Sensor = SSNOntologyModel.getOntClass(SSN_URI+"Sensor");
+		return Sensor;
+	}
+	
+	public static OntClass getSensorOutputClass(){
+		OntClass SensorOutput = SSNOntologyModel.getOntClass(SSN_URI+"SensorOutput");
+		return SensorOutput;
+	}
+	
+	public static OntClass getObservationValueClass(){
+		OntClass ObservationValue = SSNOntologyModel.getOntClass(SSN_URI+"ObservationValue");
+		return ObservationValue;
+	}
+	
+	
 	public static void main(String[] args) {
-		getSystemClass();
+	  System.out.println(getObservationValueClass());
 	}
 }
